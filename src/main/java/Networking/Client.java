32fe9e50a4faf6f032522110876b234363a87863
ChristophTheFirst <ic21b028@technikum-wien-at>
2023-01-05 18:48:30 +0100

    test über textfeld an Server verschickt, Klasse Client hinzugefügt

--- src/main/java/Networking/Client.java
+++ src/main/java/Networking/Client.java
@@ -1,5 +1,7 @@
 package Networking;
 
+import javafx.scene.control.TextField;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -13,14 +15,32 @@ public class Client {
 
 
 
+
     public Client(Socket socket,String username) {
 
 
            this.socket = socket;
           this.username = username;
+         try{
+             this.incoming = new InputStreamReader(socket.getInputStream());
+         }catch(IOException e){
+             System.out.println("could not create stream");
+
+         }
 
        }
+/*
+       public void receiveMessage(TextField textfield){
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+            while(socket.isClosed()){
 
+            }
+            }
+        }
+       }
+*/
 
     public Socket getSocket() {
         return socket;
